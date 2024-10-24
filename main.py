1af4a262339297c31d6508738d43e5e508b97614
RayyanJarral <918111@lcps.org>
2024-10-24 14:47:21 -0400

    Create main.py
    Uhh idk if this actually works, just testing!

---
+++ main.py
@@ -0,0 +1,17 @@
+!pip install SpeechRecognition
+!pip install PyAudio
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak now...")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said: " + text)
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
